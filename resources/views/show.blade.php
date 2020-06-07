f71bd49f47f6f81d3838b49af6dc86f5f498d6d0
crebro <creationduwal@gmail.com>
2020-06-07 09:35:46 +0545

    Created: AnswerController and Answer model

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -9,6 +9,13 @@
     <hr>
     <h3>More About This Question</h3>
     <p>{{$post->description}}</p>
+    <form action="{{ route( }}" method="post">
+    <div class="form-group">
+    <label for="exampleFormControlTextarea1">Give some detailed information about your question</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="description"></textarea>
+  </div>
+  <button class="btn btn-primary" type="submit">Submit Form </button>
+</form>
 </div>
 
 @endsection
